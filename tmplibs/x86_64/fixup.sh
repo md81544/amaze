b22b9e95d69e2d37d783e1b7806d79252b66e7b9
Martyn Davis <mcd@marengo.cc>
2025-02-23 13:56:25 +0000

    Updated fixup.sh for tmplibs

--- tmplibs/x86_64/fixup.sh
+++ tmplibs/x86_64/fixup.sh
@@ -14,3 +14,6 @@ install_name_tool -change  "@rpath/../Frameworks/vorbisfile.framework/Versions/A
 install_name_tool -change  "@rpath/../Frameworks/vorbis.framework/Versions/A/vorbis" "${DIR}/opt/libvorbis/lib/libvorbis.0.dylib" ./libsfml-audio.2.6.2.dylib
 install_name_tool -change  "@rpath/../Frameworks/ogg.framework/Versions/A/ogg" "${DIR}/opt/libogg/lib/libogg.0.dylib" ./libsfml-audio.2.6.2.dylib
 install_name_tool -change  "@rpath/../Frameworks/FLAC.framework/Versions/A/FLAC" "${DIR}/opt/flac/lib/libFLAC.12.dylib" ./libsfml-audio.2.6.2.dylib
+# Just in case the above doesn't work, owing to having been changed before, we update to
+# flac 14 from flac 12:
+install_name_tool -change  "${DIR}/opt/flac/lib/libFLAC.12.dylib" "${DIR}/opt/flac/lib/libFLAC.14.dylib" ./libsfml-audio.2.6.2.dylib
